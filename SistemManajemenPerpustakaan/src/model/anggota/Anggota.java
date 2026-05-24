c23d3b9155fc37a0540ebd89288ab6419712795b
Ricky123240126 <rickymhmdhrnsyh@gmail.com>
2026-05-24 20:39:11 +0700

    Add files via upload

---
+++ SistemManajemenPerpustakaan/src/model/anggota/Anggota.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.anggota;
+
+import model.Orang;
+
+/**
+ *
+ * @author Carbon
+ */
+public class Anggota extends Orang{
+    private String tipeKeanggotaan;
+    private String tanggalGabung;
+    private String status;
+    
+    public Anggota(){
+        super();
+    }
+    
+    public Anggota(int id, String nama, String email, String noTelepon, 
+                   String tipeKeanggotaan, String tanggalGabung, String status){
+        super(id, nama, email, noTelepon);
+        this.tipeKeanggotaan = tipeKeanggotaan;
+        this.tanggalGabung = tanggalGabung;
+        this.status = status;
+    }
+    
+    public Anggota(String nama, String email, String noTelepon, 
+                   String tipeKeanggotaan, String tanggalGabung, String status){
+        super(nama, email, noTelepon);
+        this.tipeKeanggotaan = tipeKeanggotaan;
+        this.tanggalGabung = tanggalGabung;
+        this.status = "aktif";
+    }
+    
+    @Override
+    public String getRole() {
+        return "Anggota"; 
+    }
+
+    // Getter & Setter
+    public String getTipeKeanggotaan() { 
+        return tipeKeanggotaan; 
+    }
+    public void setTipeKeanggotaan(String tipeKeanggotaan) {
+        this.tipeKeanggotaan = tipeKeanggotaan; 
+    }
+
+    public String getTanggalGabung() {
+        return tanggalGabung; 
+    }
+    public void setTanggalGabung(String tanggalGabung) {
+        this.tanggalGabung = tanggalGabung; 
+    }
+
+    public String getStatus() {
+        return status; 
+    }
+    public void setStatus(String status) {
+        this.status = status; 
+    }
+}
---
+++ SistemManajemenPerpustakaan/src/model/pengguna/Pustakawan.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.pengguna;
+
+import model.Orang;
+
+/**
+ *
+ * @author Carbon
+ */
+public class Pustakawan extends Orang{
+    private String username;
+    private String password;
+ 
+    public Pustakawan() {
+        super();
+    }
+ 
+    public Pustakawan(int id, String nama, String email, String noTelepon,
+                      String username, String password) {
+        super(id, nama, email, noTelepon);
+        this.username = username;
+        this.password = password;
+    }
+ 
+    public Pustakawan(String nama, String email, String noTelepon,
+                      String username, String password) {
+        super(nama, email, noTelepon);
+        this.username = username;
+        this.password = password;
+    }
+ 
+    @Override
+    public String getRole() {
+        return "Pustakawan"; 
+    }
+ 
+    public String getNamapengguna() {
+        return username; 
+    }
+    public void setNamapengguna(String username) {
+        this.username = username; 
+    }
+ 
+    public String getPassword() {
+        return password; 
+    }
+    public void setPassword(String password) {
+        this.password = password; 
+    }
+ 
+    @Override
+    public String toString() {
+        return "Pustakawan{id=" + getId() + ", nama='" + getNama() + "', namapengguna='" + username + "'}";
+    }
+}
